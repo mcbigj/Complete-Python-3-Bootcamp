88401d4701c51e60bc7e703a2474d49128f5130a
John McDaniel <john.mcdaniel@mnx.com>
2024-03-01 08:19:46 -0800

    first python repo John

---
+++ main.py
@@ -0,0 +1,21 @@
+myname = "John"
+myage = 52
+
+from random import randint
+
+print("Hi " + myname + f" you are {myage} years old")
+
+print(randint(1,200))
+
+if myage >= 52:
+    print("More than 52")
+else:  
+    print("Less than 52")
+
+mydictionary = {
+    "Name": "John",
+    "Weight": 250
+}
+
+print ("my dictionary value is" + mydictionary["Name"])
+
